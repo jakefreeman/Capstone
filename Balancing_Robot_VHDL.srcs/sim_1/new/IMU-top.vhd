b0236798600f365bf04913ba443a52b74680077e
jakefreeman <fvjake@aol.com>
2016-12-13 16:56:29 -0800

    First Commit

---
+++ Balancing_Robot_VHDL.srcs/sim_1/new/IMU-top.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/13/2016 04:47:38 PM
+-- Design Name: 
+-- Module Name: IMU-top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity IMU-top is
+--  Port ( );
+end IMU-top;
+
+architecture Behavioral of IMU-top is
+
+begin
+
+
+end Behavioral;
---
+++ Balancing_Robot_VHDL.srcs/sources_1/new/IMU.vhd
@@ -0,0 +1,54 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Jacob Freeman
+-- 
+-- Create Date: 12/13/2016 11:24:26 AM
+-- Design Name: 
+-- Module Name: IMU - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity IMU is
+    Port ( 
+			clk 			: in STD_LOGIC; --100 MHz
+      n_cs 			: in STD_LOGIC; --Active low chip select
+      sdi 			: in STD_LOGIC; --Serial data in from IMU
+      int_in 		: in STD_LOGIC; --interupt from IMU
+			data_rdy 	: in STD_LOGIC; --angle out has been calculated and is ready
+      sdo 			: out STD_LOGIC; --Serial data out to IMU
+			angle_out : out STD_LOGIC_VECTOR (15 downto 0); --16-bit angle value
+      sclk 			: out STD_LOGIC --1Mhz
+			);
+end IMU;
+
+architecture Behavioral of IMU is
+
+begin
+
+
+
+
+end Behavioral;
